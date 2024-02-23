4f169a7ca0b4e1c22844c2381a4062a0a208fdf7
Arkaitz Lopez <arkaitz.lopez@ikasle.egibide.org>
2024-02-23 21:33:30 +0100

    App_V1.2

---
+++ OnlineShop-App/ViewModels/ProductosViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ProductosViewModel.swift
+//  OnlineShop-App
+//
+//  Created by Arkaitz Lopez on 23/2/24.
+//
+
+import SwiftUI
+
+struct ProductosViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductosViewModel()
+}
